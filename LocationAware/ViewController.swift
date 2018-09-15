bd20e56098cece2a216118d13eeca49c34d3b4be
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-15 01:01:07 -0500

    Working version of app.

--- LocationAware/ViewController.swift
+++ LocationAware/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     //MARK:- IBOutlets
     @IBOutlet weak var latitude: UILabel!
@@ -22,12 +23,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var postalCode: UILabel!
     @IBOutlet weak var country: UILabel!
     
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //make our location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
+    //MARK:- Handling the location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //if we can get a location at all
+        let userLocation = locations[0]
+        
+        latitude.text = "\(userLocation.coordinate.latitude)"
+        longitude.text = "\(userLocation.coordinate.longitude)"
+        course.text = "\(userLocation.course)°"
+        speed.text = "\(userLocation.speed) meters per second"
+        altitude.text = "\(userLocation.altitude) meters"
+        
+        //MARK:- For debugging purposes... DELETE WHEN DONE!
+        print(locations)
+        
+        //MARK:- Getting the geolocation information
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                //try to get the placemark data - using ? because placemark is an optional data type until we confirm we have nearby placemarks
+                guard let placemark = placemarks?[0] else { return }
+                
+                //place the placemark data in the labels
+                if let sublocality = placemark.subLocality {
+                    self.subLocality.text = sublocality
+                    print(sublocality)
+                }
+                
+                if let subAdministrativeArea = placemark.subAdministrativeArea {
+                    self.subAdministrativeArea.text = subAdministrativeArea
+                    print(subAdministrativeArea)
+                }
+                
+                if let postalCode = placemark.postalCode {
+                    self.postalCode.text = postalCode
+                    print(postalCode)
+                }
+                
+                if let country = placemark.country {
+                    self.country.text = country
+                    print(country)
+                }
+            }
+        }
+            
+    }
 
 }
 
